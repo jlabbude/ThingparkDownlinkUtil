d106ca2a4baa53045097b98f3a0f61bffbb366db
Lucas <jlabbude@hotmail.com>
2024-04-18 18:29:09 -0300

    putting main in cold sleep

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ char *build_final_json(const char *eui_array[], const char *device_name_array[],
 
     for (int i = 0; i < eui_count; i++) {
         cJSON *device_jsonobj = cJSON_CreateObject();
-        cJSON_AddStringToObject(device_jsonobj, "name", device_name_array[i]); // Assuming "name" is the key for device names
+        cJSON_AddStringToObject(device_jsonobj, "Name", device_name_array[i]);
         cJSON_AddStringToObject(device_jsonobj, "EUI", eui_array[i]);
         cJSON_AddItemToArray(eui_json_array, device_jsonobj);
     }
@@ -22,34 +22,8 @@ char *build_final_json(const char *eui_array[], const char *device_name_array[],
     return json_str;
 }
 
-void filterJSON(const cJSON *json, const char *key, const char **eui_array, int *eui_count) {
-    if (json == NULL) return;
-
-    if (json->type == cJSON_Object) {
-        cJSON *item = cJSON_GetObjectItemCaseSensitive(json, key);
-        if (item != NULL && cJSON_IsString(item) && strcmp(item->string, key) == 0) {
-            // Add the value of the string to the EUI array
-            eui_array[(*eui_count)++] = strdup(item->valuestring);
-
-            printf("Added: %s\n", item->valuestring);
-        }
-        // Recursively call filterJSON for each child element
-        cJSON *child;
-        cJSON_ArrayForEach(child, json) {
-            filterJSON(child, key, eui_array, eui_count);
-        }
-    }
-
-    else if (json->type == cJSON_Array) {
-        cJSON *item;
-        cJSON_ArrayForEach(item, json) {
-            filterJSON(item, key, eui_array, eui_count);
-        }
-    }
-}
-
 int main() {
-    FILE *fp = fopen("JSON_output/verbose.json", "r");
+    FILE *fp = fopen("eui.json", "r");
     if (fp == NULL) {
         printf("Error opening JSON file\n");
         return 1;
@@ -82,9 +56,6 @@ int main() {
     const char *eui_array[max_eui_count];
     const char *name_array[max_eui_count];
     int eui_count = 0;
-
-    filterJSON(json, "EUI", eui_array, &eui_count);
-    filterJSON(json, "name", name_array, &eui_count);
 
     char *final_json = build_final_json(eui_array, name_array, eui_count);
     printf("%s\n", final_json);
