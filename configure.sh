1f46dc88f026cf5a3b1fccc9a6ecd57a9af14704
Lucas <jlabbude@hotmail.com>
2024-05-08 09:32:50 -0300

    added wip to configure many devices at once

---
+++ configure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+badge_ports=()
+
+while IFS= read -r -d '' port; do
+    badge_ports+=("$port")
+done < <(find /dev -name 'ttyACM*' -print0)
+
+
+configure_device() {
+    echo "$(<config.cfg)" > "$1"
+}
+
+for badge in "${badge_ports[@]}"; do
+
+    eval "$(configure_device "$badge")"
+    printf '%s\n' "$badge"
+
+done
