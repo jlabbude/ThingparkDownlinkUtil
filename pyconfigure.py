2fd3699b07f8fabea69f4f3de9d70cae150bd770
Lucas <jlabbude@hotmail.com>
2024-05-09 16:40:37 -0300

    fixed return stopping operation issue

--- pyconfigure.py
+++ pyconfigure.py
@@ -6,21 +6,28 @@ from cfgdicts import abeewaycfgdict
 
 baud_rate = 9600
 serial_port_array = glob.glob("/dev/ttyACM0")
+config_file = 'configlong.cfg'
 
 def communicate_with_serial(serial_port, baud_rate):
     with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
-        with open('config.cfg', 'rb') as config:
+        with open(config_file, 'rb') as config:
             for line in config:
                 ser.write(line.strip())
                 ser.write(b'\r')
 
-def get_config_from_cfg():
-    with open('config.cfg', 'r') as config:
-        for line in config:
-            p = re.compile("config set (.*) ")
-            match  = p.search(line)
-            if match:
-                return match.group(1)
+def get_config_from_cfg(parameter, line):
+    if parameter is not None:
+        pattern = r"config set %d (.*)" % parameter
+        p = re.compile(pattern)
+        match  = p.search(line)
+        if match:
+            return int(match.group(1))
+
+def get_config_parameter(line):
+    p = re.compile("config set (.*) ")
+    match = p.search(line)
+    if match:
+        return int(match.group(1))
 
 def parallel_process():
     threads = []
@@ -31,4 +38,6 @@ def parallel_process():
     for thread in threads:
         thread.join()
 
-print(get_config_from_cfg())
+with open(config_file, 'r') as config:
+    for line in config:
+        print(get_config_from_cfg(get_config_parameter(line), line))
